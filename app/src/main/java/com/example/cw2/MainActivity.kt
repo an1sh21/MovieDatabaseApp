f1adba65794cef9b04113fa52e6e460bb5d76edb
an1sh21 <107747127+an1sh21@users.noreply.github.com>
2025-04-18 14:00:04 +0530

    First Screen V1

---
+++ app/src/main/java/com/example/cw2/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.cw2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cw2.ui.theme.Cw2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Cw2Theme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    firstScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun firstScreen() {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Movie Database App",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            Button(
+                modifier = Modifier,
+                onClick = { /* TODO: Navigate to add movies screen */ }
+            ) {
+                Text("Add Movies to DB")
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            Button(
+                modifier = Modifier,
+                onClick = { /* TODO: Navigate to search screen */ }
+            ) {
+                Text("Search for Movies and Actors")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstScreenPreview() {
+    Cw2Theme {
+        firstScreen()
+    }
+}
